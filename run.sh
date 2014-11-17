3b2c9c2f6734d6d2a0bd47403f2b9cf82c8fe4d2
David Siaw <davidsiaw@gmail.com>
2014-11-17 19:00:36 +0900

    rearrange the init script a little

--- run.sh
+++ run.sh
@@ -3,12 +3,12 @@
 mkdir -p /srv/rails
 cp -r /srv/code /srv/rails/app
 chown -R www-data:www-data /srv/rails/app/public
-chown -R www-data:www-data /srv/rails/app/tmp
-chmod -R 777 /srv/rails/app/tmp
 cd /srv/rails/app
 mkdir -p tmp
+chown -R www-data:www-data /srv/rails/app/tmp
+chmod -R 777 /srv/rails/app/tmp
 touch log/production.log
 chmod 666 log/production.log
-rake db:migrate RAILS_ENV="production" 
 bundle install
+rake db:migrate RAILS_ENV="production" 
 /usr/bin/supervisord
