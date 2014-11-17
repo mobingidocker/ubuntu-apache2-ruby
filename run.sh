99f05c852eda327a52d458ba66d03374503822d4
David Siaw <davidsiaw@gmail.com>
2014-11-17 18:20:35 +0900

    additional startup stuff

--- run.sh
+++ run.sh
@@ -6,5 +6,9 @@ chown -R www-data:www-data /srv/rails/app/public
 chown -R www-data:www-data /srv/rails/app/tmp
 chmod -R 777 /srv/rails/app/tmp
 cd /srv/rails/app
+mkdir -p tmp
+touch log/production.log
+chmod 666 log/production.log
+rake db:migrate RAILS_ENV="production" 
 bundle install
 /usr/bin/supervisord
