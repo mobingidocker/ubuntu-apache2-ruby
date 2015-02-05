c8a12c642dd531529f99df02947964baf2fc3349
David Siaw (via Travis CI) <david.siaw@mobingi.com>
2015-02-05 23:05:08 +0900

    cat standard out

--- run.sh
+++ run.sh
@@ -9,6 +9,6 @@ chown -R www-data:www-data /srv/rails/app/tmp
 chmod -R 777 /srv/rails/app/tmp
 touch log/production.log
 chmod 666 log/production.log
-bundle install > /var/log/bundler.log
-rake db:migrate RAILS_ENV="production" > /var/log/migration.log
+bundle install 2>&1 > /var/log/bundler.log
+rake db:migrate RAILS_ENV="production" 2>&1 > /var/log/migration.log
 /usr/bin/supervisord
